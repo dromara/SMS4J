d092085d5f653cef38399d82c631aea91296953b
JIN <1970761900@qq.com>
2023-03-25 01:09:04 +0800

    创建前端项目 完成基本框架

---
+++ sms-doc/smsdoc/src/views/AboutView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="about"></div>
+</template>
---
+++ sms-doc/smsdoc/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="logo">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
+}
+</script>
+<style></style>

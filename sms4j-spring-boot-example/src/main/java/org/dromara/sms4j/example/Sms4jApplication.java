d1120dc4fdbd89579a739bc2ff103063155bd638
handy <handyplus@163.com>
2023-05-19 18:19:34 +0800

    添加示例项目

---
+++ sms4j-spring-boot-example/src/main/java/org/dromara/sms4j/example/Sms4jApplication.java
@@ -0,0 +1,18 @@
+package org.dromara.sms4j.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主类
+ *
+ * @author handy
+ */
+@SpringBootApplication
+public class Sms4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sms4jApplication.class, args);
+    }
+
+}

9f656619555b25e7b3918f7c293f56fb67c5e879
wind <wzsf1810@qq.com>
2023-05-24 19:12:46 +0800

    更改solon的AOP为原生实现

--- sms4j-solon-plugin/src/main/java/org/dromara/sms4j/solon/XPluginImpl.java
+++ sms4j-solon-plugin/src/main/java/org/dromara/sms4j/solon/XPluginImpl.java
@@ -1,7 +1,5 @@
 package org.dromara.sms4j.solon;
 
-import org.dromara.sms4j.comm.annotation.Restricted;
-import org.dromara.sms4j.solon.aop.AopAdvice;
 import org.dromara.sms4j.solon.config.SmsAutowiredConfig;
 import org.noear.solon.core.AopContext;
 import org.noear.solon.core.Plugin;
@@ -13,6 +11,6 @@ public class XPluginImpl implements Plugin {
     @Override
     public void start(AopContext context) throws Throwable {
         context.beanMake(SmsAutowiredConfig.class);
-        context.beanAroundAdd(Restricted.class, new AopAdvice(context));
+        SmsAutowiredConfig.aopContext = context;
     }
 }
--- sms4j-solon-plugin/src/main/java/org/dromara/sms4j/solon/config/SmsAutowiredConfig.java
+++ sms4j-solon-plugin/src/main/java/org/dromara/sms4j/solon/config/SmsAutowiredConfig.java
@@ -1,6 +1,7 @@
 package org.dromara.sms4j.solon.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.dromara.sms4j.api.smsProxy.SmsInvocationHandler;
 import org.dromara.sms4j.comm.config.SmsBanner;
 import org.dromara.sms4j.comm.config.SmsConfig;
 import org.dromara.sms4j.comm.config.SmsSqlConfig;
@@ -8,6 +9,7 @@ import org.dromara.sms4j.comm.constant.Constant;
 import org.dromara.sms4j.comm.delayedTime.DelayedTime;
 import org.dromara.sms4j.comm.factory.BeanFactory;
 import org.dromara.sms4j.core.SupplierSqlConfig;
+import org.dromara.sms4j.solon.aop.SolonRestrictedProcess;
 import org.dromara.sms4j.solon.utils.RedisUtils;
 import org.noear.solon.Solon;
 import org.noear.solon.Utils;
@@ -15,6 +17,7 @@ import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Condition;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
+import org.noear.solon.core.AopContext;
 import org.noear.solon.core.Props;
 
 import java.util.concurrent.Executor;
@@ -23,6 +26,8 @@ import java.util.concurrent.Executor;
 @Slf4j
 @Configuration
 public class SmsAutowiredConfig {
+
+    public static AopContext aopContext;
 
     private <T> T injectObj(String prefix, T obj) {
         //@Inject 只支持在字段、参数、类型上注入
@@ -76,12 +81,8 @@ public class SmsAutowiredConfig {
         /* 如果配置中启用了redis，则注入redis工具*/
         if (BeanFactory.getSmsConfig().getRedisCache()){
             Solon.context().wrapAndPut(RedisUtils.class);
+            SmsInvocationHandler.setRestrictedProcess(new SolonRestrictedProcess(aopContext));
             log.debug("The redis cache is enabled for sms4j");
-        }
-
-        /* 如果启用了短信限制，则注入AOP组件*/
-        if (BeanFactory.getSmsConfig().getRestricted()){
-            log.debug("SMS restriction is enabled");
         }
 
         //打印banner

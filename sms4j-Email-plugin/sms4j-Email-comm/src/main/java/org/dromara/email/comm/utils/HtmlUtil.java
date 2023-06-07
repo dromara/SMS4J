1b5f9eee047c69d341bc8b75fd04e788943a8694
wind <wzsf1810@qq.com>
2023-06-07 21:14:39 +0800

    创建e-mail模块，添加基础工具方法

---
+++ sms4j-Email-plugin/sms4j-Email-comm/src/main/java/org/dromara/email/comm/utils/HtmlUtil.java
@@ -0,0 +1,120 @@
+package org.dromara.email.comm.utils;
+
+import org.dromara.email.comm.errors.MailException;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * HtmlUtil
+ * <p> Html相关工具
+ *
+ * @author :Wind
+ * 2023/6/7  20:15
+ **/
+public class HtmlUtil {
+
+    private static final HtmlUtil htmlUtil = new HtmlUtil();
+
+    /**
+     * readHtml
+     * <p>从resource读取模板文件
+     *
+     * @param name 模板文件名
+     * @author :Wind
+     */
+    public static List<String> readHtml(String name) throws MailException {
+        try (InputStream is = htmlUtil.getClass().getResourceAsStream("/template/" + name);) {
+            return readHtml(is);
+        } catch (IOException e) {
+            throw new MailException(e);
+        }
+    }
+
+    /**
+     * readHtml
+     * <p>从自定义路径读取模板文件
+     *
+     * @param file 自定义路径file
+     * @author :Wind
+     */
+    public static List<String> readHtml(File file) throws MailException {
+        try (InputStream ip = Files.newInputStream(file.toPath());) {
+            return readHtml(ip);
+        } catch (IOException e) {
+            throw new MailException(e);
+        }
+
+    }
+
+    /**
+     * readHtml
+     * <p>从输入流读取模板文件
+     *
+     * @param inputStream 输入流
+     * @author :Wind
+     */
+    public static List<String> readHtml(InputStream inputStream) throws MailException {
+        List<String> data = new ArrayList<>();
+        if (Objects.isNull(inputStream)) {
+            throw new MailException("The template could not be found!");
+        }
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                data.add(line);
+            }
+        } catch (IOException e) {
+            throw new MailException(e);
+        }
+        return data;
+    }
+
+    /**
+     *  replacePlaceholder
+     * <p>将所包含占位符的字符串替换为固定值
+     * @param data 源数据
+     * @param parameter key为占位符名称 value为占位符应替换的值
+     * @author :Wind
+    */
+    public static List<String> replacePlaceholder(List<String> data, Map<String, String> parameter) {
+        List<String> list = new ArrayList<>();
+        for (String datum : data) {
+            for (Map.Entry<String, String> s : parameter.entrySet()) {
+                String piece = piece(s.getKey());
+                if (datum.contains(piece)){
+                    list.add(datum.replace(piece, s.getValue()));
+                }
+            }
+        }
+        return list;
+    }
+
+    /**
+     *  pieceHtml
+     * <p>将数据拼合为html
+     * @param data 需要拼合的数据
+     * @author :Wind
+    */
+    public static String pieceHtml(List<String> data){
+        StringBuilder sb = new StringBuilder();
+        for (String datum : data) {
+          sb.append(datum);
+          sb.append("\r\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     *  piece
+     * <p>将参数拼合为完整占位符
+     * @author :Wind
+    */
+    public static String piece(String parameter){
+        return "#{"+parameter+"}";
+    }
+}
---
+++ sms4j-Email-plugin/sms4j-Email-core/src/main/java/org/dromara/email/core/Sms4jEmailCoreApplication.java
@@ -0,0 +1,13 @@
+package org.dromara.email.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sms4jEmailCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sms4jEmailCoreApplication.class, args);
+    }
+
+}

e2142aaa2361922aeac90a72e5a803bf0f373898
bleachtred <bleachtred@163.com>
2023-06-13 09:55:18 +0800

    add 添加邮件附件压缩方法,支持 本地文件/目录+oss网络路径 一键打包

--- sms4j-Email-plugin/sms4j-Email-core/src/main/java/org/dromara/email/core/service/MailService.java
+++ sms4j-Email-plugin/sms4j-Email-core/src/main/java/org/dromara/email/core/service/MailService.java
@@ -1,9 +1,15 @@
 package org.dromara.email.core.service;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.lang.UUID;
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.StrUtil;
 import org.dromara.email.api.MailClient;
 import org.dromara.email.api.Parameter;
 import org.dromara.email.comm.errors.MailException;
 import org.dromara.email.comm.utils.HtmlUtil;
+import org.dromara.email.comm.utils.ZipUtils;
 import org.dromara.email.core.ReflectUtil;
 
 import javax.activation.DataHandler;
@@ -15,6 +21,10 @@ import javax.mail.Multipart;
 import javax.mail.Transport;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -22,7 +32,7 @@ import java.util.Objects;
 
 public class MailService implements MailClient {
 
-    private MailBuild mailBuild;
+    private final MailBuild mailBuild;
 
     private MailService(MailBuild mailBuild) {
         this.mailBuild = mailBuild;
@@ -48,12 +58,32 @@ public class MailService implements MailClient {
     }
 
     @Override
+    public void sendEmail(String mailAddress, String title, String body, String zipName, Map<String, String> files) {
+        try {
+            Message message = mailBuild.getMessage();
+            message.setRecipients(Message.RecipientType.TO, mailBuild.eliminate(Convert.toList(String.class, mailAddress)));
+            message.setSubject(title);
+            if (StrUtil.isNotBlank(body)) {
+                message.setText(body);
+            }
+            if (files != null && files.size() != 0) {
+                Multipart multipart = new MimeMultipart();
+                zipFiles(multipart, zipName, files);
+                message.setContent(multipart);
+            }
+            Transport.send(message);
+        } catch (MessagingException | IOException e) {
+            throw new MailException(e);
+        }
+    }
+
+    @Override
     public void sendEmail(List<String> mailAddress, String title, String body, Map<String,String> files) {
         try {
             Message message = mailBuild.getMessage();
             message.setRecipients(Message.RecipientType.TO, mailBuild.eliminate(mailAddress));
             message.setSubject(title);
-            if (Objects.isNull(body) || body.isEmpty()) {
+            if (StrUtil.isNotBlank(body)) {
                 message.setText(body);
             }
             if (files != null && files.size() != 0) {
@@ -133,6 +163,38 @@ public class MailService implements MailClient {
     }
 
     @Override
+    public void sendHtml(String mailAddress, String title, String body, String htmlName, Map<String, String> parameter, String zipName, Map<String, String> files) {
+        try {
+            Message message = mailBuild.getMessage();
+            message.setRecipients(Message.RecipientType.TO, mailBuild.eliminate(Convert.toList(String.class, mailAddress)));
+            message.setSubject(title);
+
+            Multipart multipart = new MimeMultipart("alternative");
+            //读取模板并进行变量替换
+            List<String> strings = HtmlUtil.replacePlaceholder(HtmlUtil.readHtml(htmlName), parameter);
+            //拼合HTML数据
+            String htmlData = HtmlUtil.pieceHtml(strings);
+            if (StrUtil.isNotBlank(body)) {
+                // 创建文本正文部分
+                MimeBodyPart textPart = new MimeBodyPart();
+                textPart.setText(body);
+                multipart.addBodyPart(textPart);
+            }
+            //添加附件
+            if (MapUtil.isNotEmpty(files)) {
+                zipFiles(multipart, zipName, files);
+            }
+            MimeBodyPart htmlPart = new MimeBodyPart();
+            htmlPart.setContent(htmlData, "text/html;charset=UTF-8");
+            multipart.addBodyPart(htmlPart);
+            message.setContent(multipart);
+            Transport.send(message);
+        } catch (MessagingException | IOException e) {
+            throw new MailException(e);
+        }
+    }
+
+    @Override
     public void sendHtml(List<String> mailAddress, String title, String body, String htmlName, Map<String, String> parameter, Map<String, String> files) {
         try {
             Message message = mailBuild.getMessage();
@@ -185,5 +247,17 @@ public class MailService implements MailClient {
             messageBodyPart.setFileName(k);
             multipart.addBodyPart(messageBodyPart);
         }
+    }
+
+    private void zipFiles(Multipart multipart, String zipName, Map<String, String> files) throws MessagingException, IOException {
+        // 设置附件消息部分
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        ZipUtils.zipFilePip(files, os);
+        ByteArrayInputStream stream = IoUtil.toStream(os);
+        DataSource source = new ByteArrayDataSource(stream, "application/octet-stream");
+        messageBodyPart.setDataHandler(new DataHandler(source));
+        messageBodyPart.setFileName(StrUtil.isNotBlank(zipName) ? zipName: UUID.fastUUID() + ".zip");
+        multipart.addBodyPart(messageBodyPart);
     }
 }

fe9f565ae7621fa02f99dcdf8722d67e20df9e03
wind <wzsf1810@qq.com>
2023-09-18 22:13:46 +0800

    init方法无法执行的问题

--- sms4j-spring-boot-starter/src/main/java/org/dromara/sms4j/starter/config/SmsAutowiredConfig.java
+++ sms4j-spring-boot-starter/src/main/java/org/dromara/sms4j/starter/config/SmsAutowiredConfig.java
@@ -1,9 +1,7 @@
 package org.dromara.sms4j.starter.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.dromara.sms4j.comm.constant.Constant;
 import org.dromara.sms4j.comm.delayedTime.DelayedTime;
-import org.dromara.sms4j.provider.config.SmsBanner;
 import org.dromara.sms4j.provider.config.SmsConfig;
 import org.dromara.sms4j.provider.factory.BeanFactory;
 import org.dromara.sms4j.starter.utils.ConfigUtils;
@@ -12,10 +10,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 
-import javax.annotation.PostConstruct;
 import java.util.concurrent.Executor;
 
 
@@ -30,43 +26,44 @@ public class SmsAutowiredConfig {
 
     @Bean
     @ConfigurationProperties(prefix = "sms")     //指定配置文件注入属性前缀
-    protected SmsConfig smsConfig(){
+    protected SmsConfig smsConfig() {
         return BeanFactory.getSmsConfig();
     }
 
-    /** 注入一个定时器*/
+    /**
+     * 注入一个定时器
+     */
     @Bean
     @Lazy
-    protected DelayedTime delayedTime(){
+    protected DelayedTime delayedTime() {
         return BeanFactory.getDelayedTime();
     }
 
-    /** 注入线程池*/
+    /**
+     * 注入线程池
+     */
     @Bean("smsExecutor")
     @Lazy
-    protected Executor taskExecutor(SmsConfig config){
+    protected Executor taskExecutor(SmsConfig config) {
         return BeanFactory.setExecutor(config);
     }
 
-    /** 注入一个配置文件读取工具*/
+    /**
+     * 注入一个配置文件读取工具
+     */
     @Bean("smsConfigUtil")
     @Lazy
-    protected ConfigUtils configUtil(Environment environment){
+    protected ConfigUtils configUtil(Environment environment) {
         return new ConfigUtils(environment);
     }
 
-    /** smsConfig参数意义为确保注入时smsConfig已经存在*/
+    /**
+     * smsConfig参数意义为确保注入时smsConfig已经存在
+     */
     @Bean
     @ConditionalOnProperty(prefix = "sms", name = "config-type", havingValue = "yaml")
-    protected SupplierConfig supplierConfig(SmsConfig smsConfig){
+    protected SupplierConfig supplierConfig(SmsConfig smsConfig) {
         return new SupplierConfig();
     }
 
-    @PostConstruct
-    void init() {
-        //打印banner
-        if (BeanFactory.getSmsConfig().getIsPrint()){
-            SmsBanner.PrintBanner(Constant.VERSION);
-        }
-    }
 }
--- sms4j-spring-boot-starter/src/main/java/org/dromara/sms4j/starter/config/SmsMainConfig.java
+++ sms4j-spring-boot-starter/src/main/java/org/dromara/sms4j/starter/config/SmsMainConfig.java
@@ -1,9 +1,14 @@
 package org.dromara.sms4j.starter.config;
 
 import lombok.Data;
+import org.dromara.sms4j.comm.constant.Constant;
+import org.dromara.sms4j.provider.config.SmsBanner;
+import org.dromara.sms4j.provider.factory.BeanFactory;
 import org.dromara.sms4j.starter.utils.SmsSpringUtils;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 
 @Data
@@ -18,6 +23,14 @@ public class SmsMainConfig {
     @Bean
     public SmsAutowiredConfig smsAutowiredConfig(SmsSpringUtils smsSpringUtils){
         return new SmsAutowiredConfig(smsSpringUtils);
+    }
+
+    @EventListener
+    void init(ContextRefreshedEvent event) {
+        //打印banner
+        if (BeanFactory.getSmsConfig().getIsPrint()) {
+            SmsBanner.PrintBanner(Constant.VERSION);
+        }
     }
 
 }

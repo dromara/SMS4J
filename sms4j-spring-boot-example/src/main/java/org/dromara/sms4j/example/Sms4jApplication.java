a3b7397f3c404aa506b769a5071a9d4c2c0f02b6
YYM <357133578@qq.com>
2024-01-31 10:08:04 +0800

    去除测试信息

--- sms4j-spring-boot-example/src/main/java/org/dromara/sms4j/example/Sms4jApplication.java
+++ sms4j-spring-boot-example/src/main/java/org/dromara/sms4j/example/Sms4jApplication.java
@@ -19,37 +19,6 @@ public class Sms4jApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Sms4jApplication.class, args);
-//        SmsResponse smsResponse = SmsFactory.getBySupplier(SupplierConstant.QINIU).sendMessage("135****0844", "1234");
-
-//        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
-//        linkedHashMap.put("code", "2234");
-//        SmsResponse smsResponse = SmsFactory.getBySupplier(SupplierConstant.QINIU).sendMessage("135****0844", "1752130467315859456",linkedHashMap);
-//        System.out.println(smsResponse);
-
-//        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
-//        linkedHashMap.put("code", "3234");
-//        SmsResponse smsResponse = SmsFactory.getBySupplier(SupplierConstant.QINIU).sendMessage("135****0844", linkedHashMap);
-//        System.out.println(smsResponse);
-
-//        ArrayList<String> list = new ArrayList<>();
-//        list.add("135****0844");
-//        list.add("175****5952");
-//
-//        SmsResponse smsResponse = SmsFactory.getBySupplier(SupplierConstant.QINIU)
-//                .massTexting(list, "1234");
-//        System.out.println(smsResponse);
-
-
-//        ArrayList<String> list = new ArrayList<>();
-//        list.add("135****0844");
-//        list.add("175****65952");
-//
-//        LinkedHashMap<String, String> hashMap = new LinkedHashMap<>();
-//        hashMap.put("code", "2234");
-//        SmsResponse smsResponse = SmsFactory.getBySupplier(SupplierConstant.QINIU)
-//                .massTexting(list,"1752130467315859456", hashMap);
-//        System.out.println(smsResponse);
-
     }
 
 }
